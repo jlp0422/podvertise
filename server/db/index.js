a969a48f705b0495333e5be7d05c5a92064dd4a3
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-06-12 20:07:03 -0400

    fix env var

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABAUSE_URL || 'postgres://localhost/podvertise_db', {
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/podvertise_db', {
   logging: false
 })
 
